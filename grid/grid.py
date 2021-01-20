e8a90ef2ce5d967623e3ef1d46f867a8a6f4bde0
AntoineLBT <antoine.lebout@gmail.com>
2021-01-21 00:36:37 +0100

    begin to implement grid filling function

--- grid/grid.py
+++ grid/grid.py
@@ -1,4 +1,5 @@
 from .cell import Cell
+from ship.ship import Cruiser, Escort, Torpedo, SubMarine
 
 
 class Grid:
@@ -11,3 +12,15 @@ class Grid:
         for y in range(self.height):
             for x in range(self.width):
                 self.cells.append(Cell(x, y))
+
+    def fill_grid(self):
+        self.create_ships(Cruiser, 1)
+        self.create_ships(Escort, 2)
+        self.create_ships(Torpedo, 3)
+        self.create_ships(SubMarine, 4)
+
+    def create_ships(self, ship_class: object, number_of_ship: int):
+        pass
+
+    def allocate_cells(self, ship_size: int, x_pos_start: int, y_pos_start):
+        pass
