807b15467770dc2daee5c7462ee57c1a1ae5dcef
AntoineLBT <antoine.lebout@gmail.com>
2021-01-22 18:58:31 +0100

    try funny customization for window

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from controller.grid import Grid
+from view.view import View
 
 if __name__ == "__main__":
     grid = Grid(20, 20)
     grid.create_empty_grid()
     grid.fill_grid()
     print(grid)
+    view = View()
+    view.root.mainloop()
--- view/view.py
+++ view/view.py
@@ -1,9 +1,27 @@
 import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 class View:
+
+    SIZE_CHOICE = ["15x15", "20x20", "25x25", "30x30"]
+
     def __init__(self):
         self.root = tk.Tk()
+        self.root.geometry("800x600")
+        self.root.maxsize(1000, 800)
+        self.root.title("Bataille Navale")
+        self.root.config(bg="skyblue")
 
-        c = tk.Canvas(self.root, height=100, width=100, bg="white")
-        c.pack(fill=tk.BOTH, expand=True)
+        button_frame = tk.Frame(self.root, bg="grey").pack(fill="both", padx=5, pady=5)
+        load = Image.open(
+            "/home/antoine/PycharmProjects/bataille_navale/view/reshape_bg.jpg"
+        )
+        render = ImageTk.PhotoImage(load)
+        img = tk.Label(button_frame, image=render)
+        img.image = render
+        img.pack()
+
+        ttk.Combobox(button_frame, values=View.SIZE_CHOICE).pack(padx=5, pady=5)
+        tk.Button(button_frame, text="Generate").pack(padx=5, pady=5)
