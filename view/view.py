303a88416ce7b0cdbb9c954d6e4978cc5f4c1363
AntoineLBT <antoine.lebout@gmail.com>
2021-01-27 11:21:54 +0100

    refactor view

--- view/view.py
+++ view/view.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 from controller.grid import Grid
-from controller.cell import Cell
 
 
 class View:
@@ -15,25 +14,19 @@ class View:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("800x900+900+10")
-        self.root.maxsize(1000, 900)
+        self.root.maxsize(800, 900)
         self.root.title("Bataille Navale")
         self.root.config(bg="skyblue")
 
         self.button_frame = tk.Frame(self.root, bg="skyblue")
         self.button_frame.pack(fill=tk.BOTH, padx=5, pady=5)
-        load = Image.open(
-            "/home/antoine/PycharmProjects/bataille_navale/view/reshape_bg.jpg"
-        )
-        render = ImageTk.PhotoImage(load)
-        img = tk.Label(self.button_frame, image=render)
-        img.image = render
-        img.pack()
+        self.load_image()
 
         self.list_combo = ttk.Combobox(self.button_frame, values=View.SIZE_CHOICE)
         self.list_combo.current(0)
         self.list_combo.pack(padx=5, pady=5)
-        self.list_combo.bind("<<ComboboxSelected>>", self.action)
-        tk.Button(self.button_frame, text="Generate", command=self.generate_grid).pack(
+        self.list_combo.bind("<<ComboboxSelected>>", self.generate_action_button)
+        tk.Button(self.button_frame, text="Generate", command=self.generate_visual_grid).pack(
             padx=5, pady=5
         )
 
@@ -46,7 +39,16 @@ class View:
 
         self.grid_object = Grid(0, 0)
 
-    def action(self, event):
+    def load_image(self):
+        load = Image.open(
+            "/home/antoine/PycharmProjects/bataille_navale/view/reshape_bg.jpg"
+        )
+        render = ImageTk.PhotoImage(load)
+        img = tk.Label(self.button_frame, image=render)
+        img.image = render
+        img.pack()
+
+    def generate_action_button(self, event):
         size = self.list_combo.get()
         if len(size) == 0:
             self.popup()
@@ -67,13 +69,12 @@ class View:
         pop_up.grab_set()
         self.root.wait_window(pop_up)
 
-    def generate_grid(self):
+    def generate_visual_grid(self):
         if self.chosen_size is None:
             self.popup()
             return None
 
-        for widget in self.grid_frame.winfo_children():
-            widget.destroy()
+        self.clean_grid()
 
         canvas = tk.Canvas(
             self.grid_frame,
@@ -82,27 +83,31 @@ class View:
             bg="skyblue",
         )
 
-        x_start_grid, y_start_grid = self.calculate_grid_position()
+        x_start_grid, y_start_grid = self.calculate_visual_grid_position()
         x_start_grid = int(x_start_grid)
-        y_start_grid = int(y_start_grid) - 30
+        y_start_grid = int(y_start_grid) - 30  # hardcoded padY
 
         self.initialize_grid_object()
 
+        self.create_visual_grid(canvas, x_start_grid, y_start_grid)
+
+        canvas.pack(fill=tk.BOTH, pady=0)
+
+    def create_visual_grid(self, canvas, x_start_grid, y_start_grid):
         for x, x_in_px in enumerate(
-            range(
-                x_start_grid,
-                (self.chosen_size[0] * View.CELL_SIZE) + x_start_grid,
-                View.CELL_SIZE,
-            )
-        ):
-            for y, y_in_px in enumerate(
                 range(
-                    y_start_grid,
-                    (self.chosen_size[1] * View.CELL_SIZE) + y_start_grid,
+                    x_start_grid,
+                    (self.chosen_size[0] * View.CELL_SIZE) + x_start_grid,
                     View.CELL_SIZE,
                 )
+        ):
+            for y, y_in_px in enumerate(
+                    range(
+                        y_start_grid,
+                        (self.chosen_size[1] * View.CELL_SIZE) + y_start_grid,
+                        View.CELL_SIZE,
+                    )
             ):
-
                 cell = self.grid_object.grid[y][x]
                 canvas.create_rectangle(
                     x_in_px,
@@ -111,9 +116,12 @@ class View:
                     y_in_px + View.CELL_SIZE,
                     fill=cell.color,
                 )
-        canvas.pack(fill=tk.BOTH, pady=0)
 
-    def calculate_grid_position(self):
+    def clean_grid(self):
+        for widget in self.grid_frame.winfo_children():
+            widget.destroy()
+
+    def calculate_visual_grid_position(self):
 
         grid_width = self.chosen_size[0] * View.CELL_SIZE
         x_start_grid = (View.GRID_WIDTH - grid_width) / 2
